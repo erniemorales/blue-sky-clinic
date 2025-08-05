bc53088aa6b8f0b7fa7761fbd27688cdc232063a
erniemorales <morales.ernesto@gmail.com>
2025-08-05 07:45:47 -0600

    Delete Micrositio 1  directory

--- Micrositio 1 /js/main.js
+++
@@ -1,6 +0,0 @@
-function toggleLanguage() {
-  const esElements = document.querySelectorAll('.es');
-  const frElements = document.querySelectorAll('.fr');
-  esElements.forEach(el => el.style.display = el.style.display === 'none' ? '' : 'none');
-  frElements.forEach(el => el.style.display = el.style.display === 'none' ? '' : 'none');
-}
